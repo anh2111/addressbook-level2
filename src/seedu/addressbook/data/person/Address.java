23a9a8cdb0c5d7980309a5ad39a0bc2d932a8a1b
anhhuynh <anhhuynh98@gmail.com>
2018-02-13 14:28:55 +0800

    add classes Block, Street, Unit, PostalCode

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,12 +8,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "add John Doe p/98765432 e/johnd@gmail.com a/John street, block 123, #01-01";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should follow format BLOCK, STREET, UNIT, POSTAL_CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
     public final String value;
     private boolean isPrivate;
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postal;
 
     /**
      * Validates given address.
@@ -22,11 +26,20 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
+        this.isPrivate = isPrivate;
         this.value = trimmedAddress;
+
+        String[] parsedAddress = address.split(",");
+        if (parsedAddress.length != 4) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.block = new Block(parsedAddress[0].trim());
+        this.street = new Street(parsedAddress[1].trim());
+        this.unit = new Unit(parsedAddress[2].trim());
+        this.postal = new PostalCode(parsedAddress[3].trim());
     }
 
     /**
@@ -55,5 +68,37 @@ public class Address {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    class Block {
+        public final String value;
+
+        public Block(String value) {
+            this.value = value;
+        }
+    }
+
+    class Street {
+        public final String value;
+
+        public Street(String value) {
+            this.value = value;
+        }
+    }
+
+    class Unit {
+        public final String value;
+
+        public Unit(String value) {
+            this.value = value;
+        }
+    }
+
+    class PostalCode {
+        public final String value;
+
+        public PostalCode(String value) {
+            this.value = value;
+        }
     }
 }
